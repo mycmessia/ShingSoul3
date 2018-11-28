04477e08ef2cd61e8a627f7f1e4c0baec9c73643
YuchenMei <yuchen.mei@coconutlizard.co.uk>
2018-11-28 16:06:40 +0000

    Deleted Paragon Character which we are unlikely gonna use

--- Source/ShiningSoul3/SSGameMode.cpp
+++ Source/ShiningSoul3/SSGameMode.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SSGameMode.h"
-#include "SSCharacter.h"
+#include "Character/SSCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 
